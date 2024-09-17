0dcf320c458b3bda9d0f2347285398c20d2100ff
Robiul Hasan <muhammadrobiul502@gmail.com>
2024-09-18 03:30:53 +0600

    latest

--- main.py
+++ main.py
@@ -1,3 +1,28 @@
+import sys
+
 # Variables In Python
 my_name = "Robiul Hasan"
-print("My name is", my_name)
+my_age = 26
+my_height = 5.8
+isMarried = True
+my_weight = 70
+my_eyes = "Black"
+my_teeth = "White"
+my_hair = "Black"
+
+string_format = f"Let's talk about {my_name}." # f-string
+
+# casting in python
+
+my_age = str(my_age)
+my_height = int(my_height)
+my_weight = float(my_weight)
+
+
+# conditions in python
+
+if 5 > 9:
+    print("5 is greater than 2")
+else:
+    print("5 is not greater than 2")
+
