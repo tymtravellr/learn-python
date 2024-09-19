879717c2c994b26d54bb7af03e41a45611313194
Robiul Hasan <muhammadrobiul502@gmail.com>
2024-09-19 16:40:52 +0600

    latest

--- main.py
+++ main.py
@@ -18,11 +18,43 @@ my_age = str(my_age)
 my_height = int(my_height)
 my_weight = float(my_weight)
 
+#list
 
-# conditions in python
+fruits = [ "banana", "apple", "cherry"];
+# fruits.append("orange")
+# fruits.clear()
+# fruits.insert(0, "mango")
+# fruits.pop(1)
 
-if 5 > 9:
-    print("5 is greater than 2")
-else:
-    print("5 is not greater than 2")
+# # conditions in python
+# if 5 > 9:
+#     # print("5 is greater than 2")
+# else:
+#     # print("5 is not greater than 2")
 
+# loops in python
+# for fruit in fruits:
+    # print(fruit)
+
+# for index in range(len(fruits)):
+    # print(fruits[index])
+
+# print(len(fruits)) // check length
+
+# [print(x) for x in fruits] // shorthand for loop
+
+# new_list = []
+
+# for x in fruits:
+#     if "a" in x:
+#         new_list.append(x)
+
+# print(new_list)
+# fruits.sort()
+# fruits.sort(reverse= True)
+fruits.count("banana")
+# print(fruits.count("cherry"))
+
+### ------ Tuples ------ ###
+
+my_list = ("item 1", "item 2", "item 3")
